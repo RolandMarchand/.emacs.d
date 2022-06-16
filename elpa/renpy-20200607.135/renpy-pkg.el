1755970d5c67e7d1c94ce49671fd43778bc453e8
Moowool <moowool195@gmail.com>
2022-06-16 08:13:42 -0400

    Added C programming.

--- elpa/renpy-20200607.135/renpy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from renpy.el  -*- no-byte-compile: t -*-
-(define-package "renpy" "20200607.135" "silly walks for Renpy" 'nil :commit "f2f95a72a8c842f229f80999132e8ea8ee73f6fc" :authors '(("PyTom" . "pytom@bishoujo.us")) :maintainer '("Dave Love" . "fx@gnu.org") :keywords '("languages") :url "https://github.com/billywade/renpy-mode")
